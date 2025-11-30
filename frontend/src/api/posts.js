02b143c9b498532e60985747ec32a890832ed3c2
LovroNguyen <86762968+LovroNguyen@users.noreply.github.com>
2025-11-30 13:33:21 +0700

    fix minor bug in post listing API

--- frontend/src/api/posts.js
+++ frontend/src/api/posts.js
@@ -1,7 +1,7 @@
 import { api } from "./http"
 
 export const PostAPI = {
-    list: () => api.get('/posts').then(r => r.data),
+    list: (params) => api.get('/posts', { params }).then(r => r.data),
     get: (id) => api.get(`/posts/${id}`).then(r => r.data),
     create: (payload) => api.post('/posts', payload).then(r => r.data),
     update: (id, payload) => api.put(`/posts/${id}`, payload).then(r => r.data),
